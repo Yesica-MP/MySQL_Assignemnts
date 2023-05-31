8d3c96d01aaeb5059b33b535b8e2ecc9cbd213c9
Yesica-MP <yesica.lmp@gmail.com>
2023-05-30 18:11:19 -0600

    Week10Assignment

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -11,13 +11,17 @@ import projects.service.ProjectService;
 
 
 
+
 public class ProjectsApp {
 	private Scanner scanner = new Scanner (System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	// @formatter:off
 	private List<String> operations = List.of(
-		"1) Add a project."	
+		"1) Add a project.",
+		"2) List Projects.",
+		"3) Select a Project."
 	);
 	// @formatter:on
 	
@@ -43,6 +47,14 @@ public class ProjectsApp {
 						createProject();
 						break;
 						
+					case 2:
+						listProjects();
+						break;
+						
+					case 3:
+						selectProject();
+						break;
+						
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Try again.");
 						break;
@@ -56,6 +68,24 @@ public class ProjectsApp {
 		
 	}
 
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+		
+	}
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println("   " + project.getProjectId() + ": " + project.getProjectName()));
+		
+		
+	}
 	private void createProject() {
 		String projectName = getStringInput("Enter the project name");
 		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
@@ -124,7 +154,13 @@ public class ProjectsApp {
 	System.out.println("\nThese are tha available selections. Press the enter key to quit: ");
 	
 	operations.forEach(line -> System.out.println("   " + line));
-		
+	
+	if(Objects.isNull(curProject)) {
+		System.out.println("\nYou are not working with a project");
+	}
+	else	{
+		System.out.println("\nYou are working with project: " + curProject);
+	}
 	}
 
 }
